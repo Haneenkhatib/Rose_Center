e38d5acb89bfca847a24b6841cb67b8461a7fef6
Haneenkhatib <jamalhaneen672@gmail.com>
2019-04-02 22:45:40 +0300

    relationships between models

--- app/Appointment.php
+++ app/Appointment.php
@@ -11,4 +11,12 @@ class Appointment extends Model
     protected $fillable = ['user_id', 'service_id','Date', 'Description'];
     protected $dates = ['created_at', 'updated_at','deleted_at'];
 
+    //users //appointments
+    public function users(){
+        return $this->belongsTo("App\User");
+    }
+    public function services(){
+        return $this->belongsTo("App\Appointment");
+    }
+
 }
