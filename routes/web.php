d3de0234c0e0d3f447c71135619cce73f4b26036
Esraa Dhair <esraadhair98@gmail.com>
2019-05-27 12:45:57 -0700

    Website files (route,controller,view)

--- routes/web.php
+++ routes/web.php
@@ -15,15 +15,12 @@ Route::group(['prefix' => 'controlpanel'], function () {
     Route::resource('/Services','ControlPanelControllers\ServicesController');
     Route::resource('/appointments','ControlPanelControllers\AppointmentController');
     Route::resource('/users','ControlPanelControllers\UserController');
-<<<<<<< HEAD
-});
-
-Route::get('website', function () {
-    return view('Layouts.WebsiteLayout.Layout');
-});
-=======
     Route::get('/userProfile/{id}','ControlPanelControllers\UserController@viewProfile')->name('user.profile');
     Route::resource('/Services','ControlPanelControllers\ServicesController');
-
 });
->>>>>>> 9a4d93209df570c0adc5704d98dc639dcb799f3b
+
+Route::group(['prefix' => 'website'], function () {
+    Route::resource('/Services','WebsiteControllers\ServicesController');
+});
+
+
