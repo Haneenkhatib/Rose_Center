798630d8f10cd4f5700de6a9c140cd262020d2f4
Esraa Dhair <esraadhair98@gmail.com>
2019-06-23 12:56:17 -0700

    user profile in website

--- resources/views/Layouts/WebsiteLayout/header/header_meta.blade.php
+++ resources/views/Layouts/WebsiteLayout/header/header_meta.blade.php
@@ -14,6 +14,8 @@
 <link rel="stylesheet" href="{{asset('websiteControl/css/owl.carousel.css')}}"/>
 <link rel="stylesheet" href="{{asset('websiteControl/css/style.css')}}"/>
 <link rel="stylesheet" href="{{asset('websiteControl/css/animate.css')}}"/>
+<link href="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/css/bootstrap.min.css" rel="stylesheet" id="bootstrap-css">
+<link href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css" rel="stylesheet">
 {{--<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">--}}
 
 
@@ -21,4 +23,6 @@
 <script src="{{asset('websiteControl/https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js')}}"></script>
 <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
 <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+<script src="//maxcdn.bootstrapcdn.com/bootstrap/3.3.0/js/bootstrap.min.js"></script>
+<script src="//code.jquery.com/jquery-1.11.1.min.js"></script>
 <![endif]-->
